b2b2919a209699832d77775002bda0d8f8765b90
Albin Norback <albin.nojback@gmail.com>
2023-01-12 14:36:50 +0100

    add mvp backend

--- api/blogs.js
+++ api/blogs.js
@@ -1,28 +1,25 @@
 import { connectToDatabase } from "../lib/database.js";
 
 export default async function handler(req, res) {
+	try {
+		if (req.method === "GET") {
+			const db = await connectToDatabase();
+			const blogs = db.collection("blogs");
 
-    try {
-        if (req.method === "GET") {
-
-            const db = await connectToDatabase();
-            const blogs = db.collection("blogs");
-
-            if (isNaN(req.query.timestamp)) {
-                const blogs = await blogs.find({}).toArray();   // get all blogs from database here
-                res.status(200).send(blogs);
-            }
-
-            else {
-                const blog = await blogs.findOne({"timestamp": req.query.timestamp}); // add getting blog based on timestamp from database
-                res.status(200).send(blog);
-            }
-        }
-        else {
-            res.status(501).send("only GET method is available");
-        }
-    }
-    catch {
-        res.status(500).send("error occured when getting blog(s)");
-    }
+			if (isNaN(req.query.timestamp)) {
+				const blogResult = await blogs.find({}).toArray(); // get all blogs from database here
+				res.status(200).send(blogResult);
+			} else {
+				const blog = await blogs.findOne({
+					timestamp: parseInt(req.query.timestamp),
+				}); // add getting blog based on timestamp from database
+				res.status(200).send(blog);
+			}
+		} else {
+			res.status(501).send("only GET method is available");
+		}
+	} catch (err) {
+		console.log(err);
+		res.status(500).send("error occured when getting blog(s)");
+	}
 }
--- api/form.js
+++ api/form.js
@@ -2,50 +2,46 @@ import { connectToDatabase } from "../lib/database.js";
 import { checkPassword } from "../lib/password.js";
 
 export default async function handler(req, res) {
-    try {
+	try {
+		if (req.method === "POST") {
+			if (!checkPassword(req.body.password)) {
+				res.status(401).send("incorrect password");
+				return;
+			}
+			const db = await connectToDatabase();
+			const blogs = db.collection("blogs");
+			// put recieved data into variables
+			const title = req.body.title;
+			const author = req.body.author;
+			const date = req.body.date;
+			const location = req.body.location;
+			const content = req.body.content;
+			const timestamp = Date.now();
 
-        if (req.method === "POST") {
-            if (!checkPassword(req.body.password))
-            {
-                res.status(401).send("incorrect password");
-                return;
-            }
-            const db = await connectToDatabase();
-            const blogs = db.collection("blogs");
-            // put recieved data into variables
-            const title = req.body.title;
-            const author = req.body.author;
-            const date = req.body.date;
-            const location = req.body.location;
-            const content = req.body.content;
-            const timestamp = Date.now();
+			// get and format the publishing date
+			// const yyyy = timestamp.getFullYear();
+			// let mm = timestamp.getMonth() + 1; // Months start at 0!
+			// let dd = timestamp.getDate();
 
-            // get and format the publishing date
-            // const yyyy = timestamp.getFullYear();
-            // let mm = timestamp.getMonth() + 1; // Months start at 0!
-            // let dd = timestamp.getDate();
+			// if (dd < 10) dd = '0' + dd;
+			// if (mm < 10) mm = '0' + mm;
 
-            // if (dd < 10) dd = '0' + dd;
-            // if (mm < 10) mm = '0' + mm;
-
-            // const datePublished = yyyy + '/' + mm + '/' + dd;
-            const blog = {
-                title: title,
-                author: author,
-                date: date,
-                location: location,
-                content: content,
-                timestamp: timestamp
-            };
-            await blogs.insertOne(blog);
-            res.status(200).send("blog added!");
-            
-        }
-        else {
-            res.status(501).send("only POST method is available");
-        }
-    }
-    catch {
-        res.status(500).send("error occured when posting blog");
-    }
+			// const datePublished = yyyy + '/' + mm + '/' + dd;
+			const blog = {
+				title: title,
+				author: author,
+				date: date,
+				location: location,
+				content: content,
+				timestamp: timestamp,
+			};
+			await blogs.insertOne(blog);
+			res.status(200).send("blog added!");
+		} else {
+			res.status(501).send("only POST method is available");
+		}
+	} catch (err) {
+		console.log(err);
+		res.status(500).send("error occured when posting blog");
+	}
 }
